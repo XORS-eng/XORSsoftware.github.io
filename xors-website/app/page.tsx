8102eb2b19468c3a5ada366b1479a75ebb1ba4c3
thea-exe <csokkunthea@gmail.com>
2025-11-12 02:43:12 -0500

    font sizing

--- xors-website/app/page.tsx
+++ xors-website/app/page.tsx
@@ -4,8 +4,9 @@ export default function Home() {
   return (
     <div className="flex min-h-screen h-screen flex-col items-stretch justify-start font-sans bg-white overflow-y-auto snap-y snap-mandatory scroll-smooth">
       {" "}
-      <main className="flex min-h-screen w-full max-w-3xl flex-col sm:flex-row items-center justify-center gap-12 py-32 px-16 bg-white sm:items-center snap-start shrink-0 mx-auto">
-        <div className="flex flex-col items-start gap-6 text-left max-w-md">
+      {/* SECTION 1 */}
+      <main className="min-h-screen w-full max-w-3xl grid grid-cols-1 sm:grid-cols-2 place-items-center gap-12 sm:gap-x-24 py-32 bg-white snap-start shrink-0 mx-auto">
+        <div className="flex flex-col items-center sm:items-start text-center sm:text-left gap-6 max-w-md justify-self-center sm:justify-self-end">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black">
             did u hire a bad engineer? devshop?
           </h1>
@@ -18,6 +19,7 @@ export default function Home() {
         </div>
 
         <Image
+          className="justify-self-center"
           src="/serioushamster.svg"
           alt="Serious Hamster"
           width={300}
@@ -51,8 +53,9 @@ export default function Home() {
           </a>
         </div> */}
       </main>
-      <main className="flex min-h-screen w-full max-w-3xl flex-col sm:flex-row items-center justify-center gap-12 py-32 px-16 bg-white sm:items-center snap-start shrink-0 mx-auto">
-        <div className="flex flex-col items-start gap-6 text-left max-w-md">
+      {/* SECTION 2 */}
+      <main className="min-h-screen w-full max-w-3xl grid grid-cols-1 sm:grid-cols-2 place-items-center gap-12 sm:gap-x-24 py-32  bg-white snap-start shrink-0 mx-auto">
+        <div className="flex flex-col items-start gap-6 text-left max-w-md justify-self-center sm:justify-self-end">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black">
             we can help.
           </h1>
@@ -65,6 +68,7 @@ export default function Home() {
         </div>
 
         <Image
+          className="justify-self-center"
           src="/smarthamster.svg"
           alt="Smart Hamster"
           width={300}
@@ -98,8 +102,9 @@ export default function Home() {
           </a>
         </div> */}
       </main>
-      <main className="flex min-h-screen w-full max-w-3xl flex-col sm:flex-row items-center justify-center gap-12 py-32 px-16 bg-white sm:items-center snap-start shrink-0 mx-auto">
-        <div className="flex flex-col items-start gap-6 text-left max-w-md">
+      {/* SECTION 3 */}
+      <main className="min-h-screen w-full max-w-3xl grid grid-cols-1 sm:grid-cols-2 place-items-center gap-12 sm:gap-x-24 py-32 bg-white snap-start shrink-0 mx-auto">
+        <div className="flex flex-col items-start gap-6 text-left max-w-md justify-self-center sm:justify-self-end">
           <p className="max-w-md leading-8 text-zinc-600 dark:text-zinc-400">
             <a
               href="mailto:xorssoftware@gmail.com"
@@ -111,6 +116,7 @@ export default function Home() {
         </div>
 
         <Image
+          className="justify-self-center"
           src="/happyhamster.svg"
           alt="Happy Hamster"
           width={300}
